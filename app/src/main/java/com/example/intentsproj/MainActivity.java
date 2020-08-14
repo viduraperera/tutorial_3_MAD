b0334024b19085821a74d9d96df41a5cd300d668
vidura umayanaga perera <laumayangaperera@gmail.com>
2020-08-14 10:34:25 +0530

    first calculation and updates

--- app/src/main/java/com/example/intentsproj/MainActivity.java
+++ app/src/main/java/com/example/intentsproj/MainActivity.java
@@ -5,19 +5,39 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    public static final String EXTRA_NUMBER_ONE = "com.example.intentsproj.EXTRA_NUMBER_ONE";
+    public static final String EXTRA_NUMBER_TWO = "com.example.intentsproj.EXTRA_NUMBER_TWO";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Creating the LayoutInflater instance
+        LayoutInflater li = getLayoutInflater();
 
+        //Getting the View object as defined in the customtoast.xml file
+        View layout = li.inflate(R.layout.customtoast, (ViewGroup) findViewById(R.id.custom_toast_layout));
+
+        //Creating the Toast object
+        Toast toast = new Toast(getApplicationContext());
+        toast.setDuration(Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+        toast.setView(layout);//setting the view of custom toast layout
+        toast.show();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
         Button okButton = (Button) findViewById(R.id.Okbutton);
         okButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -27,11 +47,17 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
     public void openSecondActivity(){
         EditText editNumberOne = (EditText) findViewById(R.id.NumberOneEdit);
-        String text = editNumberOne.getText().toString();
+        String firstNumber = editNumberOne.getText().toString();
+
+        EditText editNumberTwo = (EditText) findViewById(R.id.NumberTwoEdit);
+        String secondNumber = editNumberTwo.getText().toString();
 
         Intent i = new Intent(this, SecondActivity.class);
+        i.putExtra(EXTRA_NUMBER_ONE, firstNumber);
+        i.putExtra(EXTRA_NUMBER_TWO, secondNumber);
         startActivity(i);
     }
     public void displayToast(){
--- app/src/main/java/com/example/intentsproj/SecondActivity.java
+++ app/src/main/java/com/example/intentsproj/SecondActivity.java
@@ -6,13 +6,48 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+
+    int no1, no2, answer;
+    String textViewAnswer;
+    TextView result;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+
+
+
+        Intent i = getIntent();
+        String numberOne = i.getStringExtra(MainActivity.EXTRA_NUMBER_ONE);
+        String numberTwo = i.getStringExtra(MainActivity.EXTRA_NUMBER_TWO);
+
+        result = findViewById(R.id.final_result);
+        TextView updatedOne = (TextView) findViewById(R.id.numberOneUpdated);
+        TextView updatedTwo = (TextView) findViewById(R.id.numberTwoUpdated);
+
+        no1 = Integer.parseInt(numberOne);
+        no2 = Integer.parseInt(numberTwo);
+
+        updatedOne.setText(numberOne);
+        updatedTwo.setText(numberTwo);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Button plusButton = (Button) findViewById(R.id.plus);
+        plusButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                answer = no1 + no2;
+                textViewAnswer = String.valueOf(answer);
+                result.setText(no1 + " + " + no2 + " = " + answer);
+            }
+        });
 
     }
 }
